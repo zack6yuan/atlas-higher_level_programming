1e69ac6aeffdb137393a96ade40674610e3654f3
zack6yuan <zack6yuan@gmail.com>
2024-08-29 19:25:12 +0000

    task 6

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for i in range(0, 10):
+    for j in range(i + 1, 10):
+        if ((i != 8) and (i != 9)):
+            print("{0}{1}, ".format(i, j), end="")
+        else:
+            print("{0}{1}".format(i, j))
