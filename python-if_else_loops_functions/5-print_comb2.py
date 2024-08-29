5a6cb91fe50bc4a8f431ccf4e42a38e2dfd30fcb
zack6yuan <zack6yuan@gmail.com>
2024-08-29 17:54:40 +0000

    removed comma after the last number, 99

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -2,5 +2,7 @@
 for i in range(0, 100):
     if (i <= 9):
         print("0{}, ".format(i), end="")
+    elif (i == 99):
+        print("{}".format(i), end="")
     else:
         print("{}, ".format(i), end="")
