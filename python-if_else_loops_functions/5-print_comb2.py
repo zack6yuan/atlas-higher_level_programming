9ff31b21bf3409d5970350d59d97ed86a66b01b9
zack6yuan <zack6yuan@gmail.com>
2024-08-29 17:50:02 +0000

    only one loop

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 10):
-    print("0{},".format(i), end="")
-for i in range(10, 100):
-    print("{},".format(i), end="")
+for i in range(0, 100):
+    if (i <= 9):
+        print("0{},".format(i), end="")
+    else:
+        print("{},".format(i), end="")
