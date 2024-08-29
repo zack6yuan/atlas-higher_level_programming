ae8aa06e132622c7ff144087c8044be4026dc263
zack6yuan <zack6yuan@gmail.com>
2024-08-29 18:05:43 +0000

    mabye?

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    if (i <= 9):
-        print("0{}, ".format(i), end="")
-    elif (i == 99):
-        print("{}".format(i), end="")
-    else:
-        print("{}, ".format(i), end="")
+    print("{:02}, ".format(i), end="")
+if (i == 99):
+    print("{}".format(i), end="")
