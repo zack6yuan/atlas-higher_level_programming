ec73613adc7d8bfcc0fbb1d756e8e9dbfc4a1257
zack6yuan <zack6yuan@gmail.com>
2024-08-30 18:33:13 +0000

    mabye?

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
 import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) - 1 == 0:
+        print("{} arguments: {}".format(0, sys.argv))
+    elif len(sys.argv) - 1 == 1:
+        print("{} argument: {}".format(1, sys.argv))
+    else:
+        print("{} arguments: ".format(len(sys.argv)))
