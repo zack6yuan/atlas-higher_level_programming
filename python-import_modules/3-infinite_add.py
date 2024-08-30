0c8a05c8795bcac8233132e92f2cb1fd839db5a4
zack6yuan <zack6yuan@gmail.com>
2024-08-30 18:14:10 +0000

    forgot sys

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -4,5 +4,5 @@ import sys
 if __name__ == "__main__":
     total_sum = 0
     for i in range(1, len(sys.argv)):
-        total_sum += int(argv[i])
+        total_sum += int(sys.argv[i])
     print(total_sum)
