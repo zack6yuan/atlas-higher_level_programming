c7a85555d9e9e176dc644b2c28d7fbb5adc8c528
zack6yuan <zack6yuan@gmail.com>
2024-08-30 18:45:24 +0000

    changes

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,9 +2,10 @@
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) - 1 == 0:
-        print("{} arguments: {}".format(0, sys.argv))
-    elif len(sys.argv) - 1 == 1:
-        print("{} argument: {}".format(1, sys.argv[1:]))
+    digit = len(sys.argv) - 1
+    if digit == 0:
+        print("{} arguments: {}".format(digit, sys.argv[0]))
+    elif digit == 1:
+        print("{} argument: {}".format(digit, sys.argv[1:]))
     else:
-        print("{} arguments: {}".format(len(sys.argv) - 1, sys.argv[1:]))
+        print("{} arguments: {}".format(digit, sys.argv[1:]))
