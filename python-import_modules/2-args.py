d1a0718e19b26e47876a027aeb3f79401f32d26f
zack6yuan <zack6yuan@gmail.com>
2024-08-30 18:38:11 +0000

    access each element starting at 1

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
     if len(sys.argv) - 1 == 0:
         print("{} arguments: {}".format(0, sys.argv))
     elif len(sys.argv) - 1 == 1:
-        print("{} argument: {}".format(1, sys.argv))
+        print("{} argument: {}".format(1, sys.argv[1:]))
     else:
-        print("{} arguments: {}".format(len(sys.argv), sys.argv))
+        print("{} arguments: {}".format(len(sys.argv) - 1, sys.argv[1:]))
