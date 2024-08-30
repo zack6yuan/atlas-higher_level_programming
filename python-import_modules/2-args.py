335af78f2022b84071f016a9173f5f1ddcdb4931
zack6yuan <zack6yuan@gmail.com>
2024-08-30 18:58:16 +0000

    this took forever

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 import sys
-
 if __name__ == "__main__":
     digit = len(sys.argv) - 1
     if digit == 0:
-        print("{} arguments: {}".format(digit, sys.argv[0]))
+        print("{}".format("0 arguments."))
     elif digit == 1:
         print("{} argument: {}".format(digit, sys.argv[1:]))
     else:
         print("{} arguments: {}".format(digit, sys.argv[1:]))
+    for i in range(1, len(sys.argv)):
+        print("{}: {}".format(i, sys.argv[i]))
