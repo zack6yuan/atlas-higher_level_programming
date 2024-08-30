9f4522b9f22a4cdb74eb56129ef7cd0b8db0a08d
zack6yuan <zack6yuan@gmail.com>
2024-08-30 21:53:02 +0000

    prototype error

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def print_reversed_list_integer(my_list[]):
+def print_reversed_list_integer(my_list=[]):
     reverse = my_list[::-1]
     for digit in (reverse):
         print("{:d}".format(digit))
