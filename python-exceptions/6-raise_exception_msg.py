c84c5e9a30db00dc214177f17296c061c729f5ea
zack6yuan <zack6yuan@gmail.com>
2024-09-02 04:01:42 +0000

    changes

--- python-exceptions/6-raise_exception_msg.py
+++ python-exceptions/6-raise_exception_msg.py
@@ -4,6 +4,7 @@ def safe_print_list(my_list=[], x=0):
     for j in range (x):
         try:
             print("{}".format(my_list[j]), end="")
+            count += 1
         except IndexError:
             break
     print()
