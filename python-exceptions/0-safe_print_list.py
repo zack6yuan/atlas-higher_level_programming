a799a9388a2258895baee3089fd958bed1771d99
zack6yuan <zack6yuan@gmail.com>
2024-09-02 04:05:01 +0000

    whitespace

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     count = 0
-    for j in range (x):
+    for j in range(x):
         try:
             print("{}".format(my_list[j]), end="")
             count += 1
