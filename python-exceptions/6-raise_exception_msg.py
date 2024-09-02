d66647f0c5a8eb2651479ca016b658214308d04a
zack6yuan <zack6yuan@gmail.com>
2024-09-02 03:59:47 +0000

    task 6 complete

--- python-exceptions/6-raise_exception_msg.py
+++ python-exceptions/6-raise_exception_msg.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
-def raise_exception_msg(message=""):
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    for j in range (x):
+        try:
+            print("{}".format(my_list[j]), end="")
+        except IndexError:
+            break
+    print()
+    return count
