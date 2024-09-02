74804ab65146844b9de67861de3fdbef2c3f0858
zack6yuan <zack6yuan@gmail.com>
2024-09-02 01:25:00 +0000

    key error

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     tail_digit = abs(number) % 10
-    print("{ }".format(number % 10), end="")
+    print("{}".format(number % 10), end="")
     return (tail_digit)
