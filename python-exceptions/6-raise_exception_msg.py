4367c7028f8f25c8254589f69db5b454e1b6950f
zack6yuan <zack6yuan@gmail.com>
2024-09-02 04:05:56 +0000

    NameError

--- python-exceptions/6-raise_exception_msg.py
+++ python-exceptions/6-raise_exception_msg.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
-    count = 0
-    for j in range (x):
-        try:
-            print("{}".format(my_list[j]), end="")
-            count += 1
-        except IndexError:
-            break
-    print()
-    return count
+def raise_exception_msg(message=""):
+    raise NameError(message)
