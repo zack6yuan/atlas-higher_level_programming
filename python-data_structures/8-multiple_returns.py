ceaa77f5b0845d4f968ebcfa7ccb17861ea8ba45
zack6yuan <zack6yuan@gmail.com>
2024-09-02 01:15:49 +0000

    commit

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
+    x = len(sentence)
+    if x > 0:
+        y = sentence[0]
+    else:
+        y = None
+    return (x, y)
+    
--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,2 +1,5 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
+    if (len(my_list) == 0):
+        Return None;
+    elif (my_list > 0):
