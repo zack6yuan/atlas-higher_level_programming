1f26802f8317aba6c3ebfaa1595ee20199acd33b
zack6yuan <zack6yuan@gmail.com>
2024-09-02 03:27:19 +0000

    changes

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -2,8 +2,5 @@
 def search_replace(my_list, search, replace):
     list2 = []
     for i in my_list:
-        if (i == search):
-            new_list.append(replace)
-        else:
-            new_list.append(i)
+        list2.append(replace if i == search else i)
     return (list2)
