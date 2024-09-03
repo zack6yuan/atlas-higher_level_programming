8b1b121352454ba5d9093e3882d3a1d44cfbc4c5
zack6yuan <zack6yuan@gmail.com>
2024-09-03 01:01:07 +0000

    whitespace removed

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -4,6 +4,6 @@ def safe_print_division(a, b):
         quo = a / b
     except ZeroDivisionError:
         quo = None
-    finally: 
+    finally:
         print("Inside result: {}".format(quo))
     return quo
