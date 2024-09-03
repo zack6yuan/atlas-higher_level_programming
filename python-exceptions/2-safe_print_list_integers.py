4540500c8f36b36bc5cc710c4eb6467127a9a2fc
zack6yuan <zack6yuan@gmail.com>
2024-09-03 00:52:30 +0000

    moved print and return out of loop

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -7,5 +7,5 @@ def safe_print_list_integers(my_list=[], x=0):
             int_count +=1
         except (TypeError, ValueError):
             continue
-        print()
-        return (int_count)
+    print()
+    return (int_count)
