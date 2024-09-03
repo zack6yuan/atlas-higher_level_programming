bfc74169f525d303517c9e072065c7415a648c10
zack6yuan <zack6yuan@gmail.com>
2024-09-03 01:12:34 +0000

    quo was not initialized

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -2,6 +2,7 @@
 def list_division(my_list_1, my_list_2, list_length):
     div_list = []
     for i in range(list_length):
+        quo = 0
         try:
             quo = my_list[1] / my_list[2]
         except ZeroDivisionError:
