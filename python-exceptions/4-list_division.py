a89c0a01a83cdd2de30f11d55b9053283c31b1e2
zack6yuan <zack6yuan@gmail.com>
2024-09-03 01:14:09 +0000

    small mistakes

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -4,7 +4,7 @@ def list_division(my_list_1, my_list_2, list_length):
     for i in range(list_length):
         quo = 0
         try:
-            quo = my_list1[i] / my_list2[i]
+            quo = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
             print("division by 0")
             quo = 0
