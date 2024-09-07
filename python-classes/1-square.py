814233cdda99001a0c26ddec26154e85efd23ac2
zack6yuan <zack6yuan@gmail.com>
2024-09-07 00:29:57 +0000

    task 1 [complete]

---
+++ python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" This is a module that creates a square with instintation "size" """
+
+
+class Square:
+    """ Class "square" with instintation "size" """
+    def __init__(self, size)
+    self.__size = size
