7bb6f8b9c06b8726e82c7b43e0d0ee8142462e7c
zack6yuan <zack6yuan@gmail.com>
2024-09-08 23:17:51 +0000

    indentation

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -7,11 +7,11 @@ class Square:
     """ Raises TypeError: if size is not type int """
     """ Raises ValueError: if size < 0 """
     def __init__(self, size=0):
-    if not isinstance(size, int):
-        raise TypeError("size must be an integer")
-    elif (size < 0):
-        raise ValueError("size must be >= 0")
-    self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = size
 
 @property
 def size(self, value):
