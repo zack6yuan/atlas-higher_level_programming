230d733e7f813c26d4381499642ee0f019f05d83
zack6yuan <zack6yuan@gmail.com>
2024-09-08 21:52:05 +0000

    task 3 complete

---
+++ python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Module that defines a class "square" """
+
+
+class Square:
+    """ Class square with instantation size """
+    def __init__(self, size=0):
+        """ Raises Type Error: if size is not type "int" """
+        """ Raises ValueError: if size < 0 """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """ Returns the area of the square """
+        return (self.__size ** 2)
