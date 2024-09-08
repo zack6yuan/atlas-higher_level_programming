c9befb00dc9684b5ab7d304cc88972789b0739ab
zack6yuan <zack6yuan@gmail.com>
2024-09-08 21:35:35 +0000

    task 2 complete

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
-""" Module that defines a class "square" based on "1-square.py" """
+""" Module that defines a class "square" """
 
 
 class Square:
-    """ 
+    """ Class "square" with instantiation "size" """
+    def __init__(self, size=0):
+        """ Raises ValueError: if size < 0 """
+        """ Raises Type Error: if size is not type "int" """
+        if (size < 0):
+            raise ValueError("size must be >= 0")
+        elif type(size) is not int:
+            raise TypeError("size must be an integer")
