7f293033540e22b163731ed278aef106aa3bd7b3
zack6yuan <zack6yuan@gmail.com>
2024-09-08 23:17:16 +0000

    updates

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -4,8 +4,14 @@
 
 class Square:
     """ Class "square" with instantation "size" """
+    """ Raises TypeError: if size is not type int """
+    """ Raises ValueError: if size < 0 """
     def __init__(self, size=0):
-        self.__size = size
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    elif (size < 0):
+        raise ValueError("size must be >= 0")
+    self.__size = size
 
 @property
 def size(self, value):
