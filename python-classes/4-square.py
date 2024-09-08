b86945ad1538b02c1065bd3fade61e08dc2d3b81
zack6yuan <zack6yuan@gmail.com>
2024-09-08 22:29:06 +0000

    task 4 complete

---
+++ python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Module that defines a class "square" """
+
+
+class Square:
+    """ Class "square" with instantation "size" """
+    def __init__(self, size=0):
+        self.__size = size
+
+@property
+def size(self, value):
+    """ Retrieves size of the square """
+    """ Returns size of the square """
+    return (self.__size)
+
+@size.setter
+def size(self, value):
+    """ Sets the size of the square """
+    """ Raises Type Error: if size is not type "int" """
+    """ Raises ValueError: if size < 0 """
+    if not isinstance(value, int)
+        raise TypeError("size must be an integer")
+    elif (size < 0):
+            raise ValueError("size must be >= 0")
+    self.__size = size
+
+def area(self):
+    """ Retrieves area of the square """
+    """ Returns area of the square """
+    return (self.__size ** 2)
