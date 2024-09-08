db18814c87931a9eb309935cf7edebf7514d87e2
zack6yuan <zack6yuan@gmail.com>
2024-09-08 21:36:57 +0000

    changes

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -11,3 +11,5 @@ class Square:
             raise ValueError("size must be >= 0")
         elif type(size) is not int:
             raise TypeError("size must be an integer")
+        else:
+            self.__size = size 
