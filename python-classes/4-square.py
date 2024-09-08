b8422521813a6e78509abcd0aff5a044a5bae733
zack6yuan <zack6yuan@gmail.com>
2024-09-08 22:29:20 +0000

    indentation

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -21,7 +21,7 @@ def size(self, value):
     if not isinstance(value, int)
         raise TypeError("size must be an integer")
     elif (size < 0):
-            raise ValueError("size must be >= 0")
+        raise ValueError("size must be >= 0")
     self.__size = size
 
 def area(self):
