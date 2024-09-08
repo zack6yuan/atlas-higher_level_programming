b8061aa1d8ed140dd0d3034a4b5993e52a029f10
zack6yuan <zack6yuan@gmail.com>
2024-09-08 22:30:43 +0000

    changes

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -22,7 +22,7 @@ def size(self, value):
         raise TypeError("size must be an integer")
     elif (size < 0):
         raise ValueError("size must be >= 0")
-    self.__size = size
+    self.__size = value
 
 def area(self):
     """ Retrieves area of the square """
