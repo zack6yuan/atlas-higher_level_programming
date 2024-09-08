df52dfe1d818f625d71845b24c85a255a5273900
zack6yuan <zack6yuan@gmail.com>
2024-09-08 21:54:46 +0000

    changes

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -3,7 +3,7 @@
 
 
 class Square:
-    """ Class square with instantation size """
+    """ Class "square" with instantation size """
     def __init__(self, size=0):
         """ Raises Type Error: if size is not type "int" """
         """ Raises ValueError: if size < 0 """
@@ -11,7 +11,7 @@ class Square:
             raise TypeError("size must be an integer")
         elif (size < 0):
             raise ValueError("size must be >= 0")
-
+        self.__size = size
     def area(self):
         """ Returns the area of the square """
         return (self.__size ** 2)
