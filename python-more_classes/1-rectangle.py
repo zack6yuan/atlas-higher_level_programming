b27742bb278126ff7411bae98849c1b259dc4b21
zack6yuan <zack6yuan@gmail.com>
2024-09-09 20:55:19 +0000

    changes

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -23,7 +23,7 @@ class Rectangle:
         """ Raises ValueError: if width < 0 """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif (width < 0):
+        elif (value < 0):
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -39,6 +39,6 @@ class Rectangle:
         """ Raises ValueError: if height < 0 """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif (height < 0):
+        elif (value < 0):
             raise ValueError("height must be >= 0")
         self.__height = value
