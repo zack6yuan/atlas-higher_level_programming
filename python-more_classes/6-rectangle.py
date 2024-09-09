29d89ad9e063921f11a3d592fefdd202096413ef
zack6yuan <zack6yuan@gmail.com>
2024-09-09 23:59:28 +0000

    changes

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -75,3 +75,4 @@ class Rectangle:
     def __del__(self):
         """Method: prints message when an instance of Rectangle is deleted """
         print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
