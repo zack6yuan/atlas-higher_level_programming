b3686094a46293024e45bc63f8e1347d4554fe2d
zack6yuan <zack6yuan@gmail.com>
2024-09-10 00:00:39 +0000

    whitespace

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -5,7 +5,7 @@
 class Rectangle:
     """ Rectangle class """
     number_of_instances = 0
-    
+
     def __init__(self, width=0, height=0):
         """ Method: New rectangle is created """
         self.width = width
