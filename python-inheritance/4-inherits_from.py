e158a9958cccf831193cbfa01d7ab4c3954cf74d
zack6yuan <zack6yuan@gmail.com>
2024-09-15 23:32:59 +0000

    whitespace

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-""" Module that returns "True" if the object is an instance of a 
+""" Module that returns "True" if the object is an instance of a
 class that inherited from the specified class, else "False" """
 
 
 def inherits_from(obj, a_class):
-    """ Method: returns value based on instance of class """
+    """ Method: returns value based on instance of class,
+    Checks if "obj" is a subclass of "a_class" """
     return (issubclass(type(obj), (a_class)) and (type(obj) is not (a_class)))
