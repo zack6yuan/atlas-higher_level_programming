c89d110c556c06a1ca6086ac339b880a06b04499
zack6yuan <zack6yuan@gmail.com>
2024-09-15 23:25:09 +0000

    whitespace

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Module that returns "True" is the object is an instance of a 
+""" Module that returns "True" is the object is an instance of a
 class that inherited from, the specified class, else "False" """
 
 
