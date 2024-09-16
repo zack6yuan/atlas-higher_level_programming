b7c894e61c78ed39daf557f906ea8e26b7e4f6ec
zack6yuan <zack6yuan@gmail.com>
2024-09-16 22:01:45 +0000

    changed mode

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -5,5 +5,5 @@ import json
 
 def load_from_json_file(filename):
     """" Method: creates an object from a JSON file """
-    with open(filename, "w") as file:
+    with open(filename) as file:
         return (json.load(file))
