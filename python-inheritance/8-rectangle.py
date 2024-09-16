df46e36c2f34b3eec71897a9c2b2c601e4151a2f
zack6yuan <zack6yuan@gmail.com>
2024-09-16 00:02:30 +0000

    task 8 complete

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Module that defines a class "Rectangle" 
+    Inherits from "7-base_geometry.py" """
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ created class "Rectangle" """
+    """ Inherits from "7-base_geometry.py" """
+    def __init__(self, width, height):
+        """ Method: width and height (positive integers)
+        validated by "integer_validator" """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
