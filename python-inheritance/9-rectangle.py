238b68396e14cbaa56315ef27146270bc00877d3
zack6yuan <zack6yuan@gmail.com>
2024-09-16 00:09:50 +0000

    forgot colon

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -16,7 +16,7 @@ class Rectangle(BaseGeometry):
         self.integer_validator("height", height)
         self.__height = height
 
-    def area(self)
+    def area(self):
         """ Returns area of rectangle """
         return ((self.__width) * (self.__height))
 
