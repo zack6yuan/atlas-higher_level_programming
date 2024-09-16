0085c3f0ce7728cf6f1c72c0edb83937377b8de4
zack6yuan <zack6yuan@gmail.com>
2024-09-16 21:32:42 +0000

    change prototype

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -2,7 +2,7 @@
 """ Module for "write to a file" """
 
 
-def read_file(filename=""):
+def write_file(filename="", text=""):
     """ Method: writes to a text file + returns # of chars written """
     with open(filename, "w", encoding="utf-8") as file:
         return (file.write(text))
