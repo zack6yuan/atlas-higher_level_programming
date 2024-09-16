31b5317acf0144446b57890f41c5c1117df4e153
zack6yuan <zack6yuan@gmail.com>
2024-09-16 22:08:55 +0000

    task 8 complete

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Module for "Class to JSON" """
+
+
+def class_to_json(obj):
+    """ Method: returns the dictionary description with
+    simple data structure (list, dictionary, string, integer
+    and boolean) for JSON serialization of an object:"""
+    return (obj.__dict__)
