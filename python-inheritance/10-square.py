d78ce5970289d72517e86d30f04f7b794edc27b3
zack6yuan <zack6yuan@gmail.com>
2024-09-16 00:17:38 +0000

    forgot the import

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Module that defines a class "Square"
 that inherits from Rectangle (9-rectangle.py) """
+Rectangle = __import__("9-rectangle.py").Rectangle
 
 
 class Square(Rectangle):
