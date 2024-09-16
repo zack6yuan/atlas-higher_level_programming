9e83a49e7200052c89652b3799566d416724bc55
zack6yuan <zack6yuan@gmail.com>
2024-09-16 00:18:35 +0000

    no file extension for import

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Module that defines a class "Square"
 that inherits from Rectangle (9-rectangle.py) """
-Rectangle = __import__("9-rectangle.py").Rectangle
+Rectangle = __import__("9-rectangle").Rectangle
 
 
 class Square(Rectangle):
