5e9a71675010a04dd5084e239797ce9183490602
zack6yuan <zack6yuan@gmail.com>
2024-09-16 00:21:23 +0000

    task 11 complete

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Module that defines a class "Square"
+that inherits from Rectangle (9-rectangle.py) """
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """ Class "Square" is created"
+    inherits from "Rectangle """
+    def __init__(self, size):
+        """ instantiation with "size" """
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """ Retreives the area
+        Returns the area """
+        return (self.__size * self.__size)
+
+    def __str__(self):
+        """ Returns width and height according to format """
+        return ("[Square] {}/{}".format((self.__size), (self.__size)))
