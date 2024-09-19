0679dfcd95da0861b8b94a593f5799d1ccff6266
zack6yuan <zack6yuan@gmail.com>
2024-09-19 17:08:19 +0000

    started base

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Module that defines class "Base" """
+
+
+class Base:
+    """ Method: initialize "Base" class """
+     __nb_objects = 0 # PRVIA (Base Object Counter)
+
+    def __init__(self, id=None):
+        """ Method: check id status and assign accordingly """
+        if not isinstance(id, None):
+            self.id = id # Assign "id"
+        else:
+            Base.__nb_objects += 1 # Increment object counter
+            self.id = Base.__nb_objects # Assign "id" with new value
