526c306014181c61e917b914b1dab4564714cc46
zack6yuan <zack6yuan@gmail.com>
2024-09-19 17:46:39 +0000

    spaces between comments

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -5,12 +5,12 @@ import json
 
 class Base:
     """ Method: initialize "Base" class """
-    __nb_objects = 0 # PRVIA (Base Object Counter)
+    __nb_objects = 0  # PRVIA (Base Object Counter)
 
     def __init__(self, id=None):
         """ Method: check id status and assign accordingly """
         if id is not None:
-            self.id = id # Assign "id"
+            self.id = id  # Assign "id"
         else:
-            Base.__nb_objects += 1 # Increment object counter
-            self.id = Base.__nb_objects # Assign "id" with new value
+            Base.__nb_objects += 1  # Increment object counter
+            self.id = Base.__nb_objects  # Assign "id" with new value
