4b62cb01954a244eb6287e293a588aeb1c4123b6
zack6yuan <zack6yuan@gmail.com>
2024-09-20 19:52:53 +0000

    added blank line

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -2,6 +2,7 @@
 """ Module "Rectangle" that inherits from "Base" """
 from models.base import Base
 
+
 class Rectangle(Base):
     """ Class "Rectangle" that inherits from "Base" class """
     def __init__(self, width, height, x=0, y=0, id=None):
