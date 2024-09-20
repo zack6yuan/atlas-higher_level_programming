6b51d7321659d490def6cd27c7fc993014a7c9bd
zack6yuan <zack6yuan@gmail.com>
2024-09-20 19:06:52 +0000

    start rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python3
 """ Module "Rectangle" that inherits from "Base" """
-from base import Base
+from models.base import Base
 
 class Rectangle(Base):
+    """ Class "Rectangle" that inherits from "Base" class """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """ Initialize Rectangle class
+
+        Attributes:
+            (self) reference to new object
+            (width) type "int" representing rectangle width
+            (height) type "int" representing rectangle height
+            (x) type "int" representing x-coordinate of position
+            (y) type "int" representing y-coordinate of position
+            (id) used to call super class
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
