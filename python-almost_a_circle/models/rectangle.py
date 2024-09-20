efb3874bed97906d54f0872bb093664aa239df17
zack6yuan <zack6yuan@gmail.com>
2024-09-20 19:41:56 +0000

    update getter and setters

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -44,3 +44,39 @@ class Rectangle(Base):
             """ Retrieves the y-coordinate of the rectangle
             Returns the y-coordinate of the rectangle """
             return (self.__y)
+
+        @width.setter
+        def width(self, value):
+            """ Sets the width of the rectangle """
+            if not isinstance(value, int):
+                raise TypeError("width must be an integer")
+            if (width <= 0):
+                raise ValueError("width must be > 0")
+            self.__width = value
+
+        @height.setter
+        def height(self, value):
+            """ Sets the height of the rectangle """
+            if not isinstance(value, int):
+                raise TypeError("height must be an integer")
+            if (height <= 0):
+                raise ValueError("height must be > 0")
+            self.__height = height
+
+        @x.setter
+        def x(self, value)
+            """ Sets the x-coordinate of the rectangle """
+            if not isinstance(value, int):
+                raise TypeError("x must be an integer")
+            if (x < 0):
+                raise ValueError("x must be >= 0")
+            self.__x = value
+
+        @y.setter
+        def y(self, value)
+            """ Sets the y-coordinate of the rectangle """
+            if not isinstance(value, int):
+                raise TypeError("y must be an integer")
+            if (y < 0):
+                raise ValueError("y must be >= 0")
+            self.__y = value
