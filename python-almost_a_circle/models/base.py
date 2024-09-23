8f68b1e034ef8d0c60c7bc0806651388f0f6f283
zack6yuan <zack6yuan@gmail.com>
2024-09-23 19:33:52 +0000

    update base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ Module that defines class "Base" """
+import json
 
 class Base:
     """ Method: initialize "Base" class """
@@ -12,3 +13,11 @@ class Base:
         else:
             Base.__nb_objects += 1  # Increment object counter
             self.id = Base.__nb_objects  # Assign "id" with new value
+
+@staticmethod
+def to_json_string(list_dictionaries):
+    """ Method: returns the JSON string
+    representation of "list_dictionaries" """
+    if (list_dictionaries) is None or (list_dictionaries) == []:
+        return "[]"
+    return json.dumps(list_dictionaries)
