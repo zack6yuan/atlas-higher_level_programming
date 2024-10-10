a3cdf7c7cf4dd865b6ad629ee854bbbfec1aa1b6
Zack Yuan <zack6yuan@gmail.com>
2024-10-10 00:50:47 -0500

    update comment.

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,3 @@
 -- Creates the database "hbtn_oc_0" in MySql server.
--- If the database already exists, the script should not fail
+-- If the database already exists, the script should not fail.
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
