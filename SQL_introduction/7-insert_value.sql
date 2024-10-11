3477b721420cd3c72fcc7f6d3268417e9bad4437
zack6yuan <zack6yuan@gmail.com>
2024-10-11 22:56:48 +0000

    update task 7.

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
 -- Inserts a new row in the table "first_table"
 -- From the database "hbtn_0c_0" in MySQL.
-INSERT INTO first_tabld (id, name) VALUES (89, "Best School");
+INSERT INTO first_table (id, name) VALUES (89, "Best School");
