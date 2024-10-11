501214e7a13f9f141c0c15b1c23889aa7fa38503
zack6yuan <zack6yuan@gmail.com>
2024-10-11 23:57:54 +0000

    wrong file.

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,1 +1,3 @@
--- 
+-- Lists all records of the table "second_table" 
+-- From the database "0_htbn0c_0" in your MySQL server.
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
