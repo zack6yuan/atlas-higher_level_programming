80d07f54ab9ddb609c8d1542e450783f99cd5992
zack6yuan <zack6yuan@gmail.com>
2024-10-11 23:56:46 +0000

    update task 10.

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
 -- Lists all records of the table "second_table" 
 -- From the database "0_htbn0c_0" in your MySQL server.
-SELECT score, name FROM second_table ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
