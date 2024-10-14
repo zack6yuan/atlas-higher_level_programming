b0e76556688b04e47bbb855e3e2298b91c093fdf
zack6yuan <zack6yuan@gmail.com>
2024-10-14 01:48:25 +0000

    i can't spell apparently.

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- Creates database "hbtn_0d_2" and the user "user_0d_2"
 CREATE DATABASE IF NOT EXISTS htbn_0d_2;
-CREATE USER IF NOT EXISETS 'user_0d_2' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_2' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2 * TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
