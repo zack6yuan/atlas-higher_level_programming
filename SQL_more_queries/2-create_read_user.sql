dbfe2c3d91b7dc632bb2b322445673a04c0a798a
zack6yuan <zack6yuan@gmail.com>
2024-10-14 01:46:52 +0000

    update task 2.

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- Creates database "hbtn_0d_2" and the user "user_0d_2"
-CREATE DATABASE IF NOT EXISTS 'htbn_0d_2'@'localhost';
+CREATE DATABASE IF NOT EXISTS htbn_0d_2;
 CREATE USER UF NOT EXISETS 'user_0d_2' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON 'hbtn_0d_2' TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2 * TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
