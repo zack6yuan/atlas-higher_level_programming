0c714286942fbba2b1416dad0f83ef805eceaf80
zack6yuan <zack6yuan@gmail.com>
2024-10-14 01:36:20 +0000

    update task 1.

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,1 +1,3 @@
--- Script that create
+-- Script that creates the MySQL user: user_0d_1
+CREATE USER "user_0d_1"@"localhost" IDENTIFIED BY "user_0d_1_pwd"; 
+GRANT ALL PRIVILEGES ON " ." TO "user_0d_1"@"localhost";
