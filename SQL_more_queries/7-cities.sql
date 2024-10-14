8c1d0725a37bba9df8404dcec37cae2955b9be3b
zack6yuan <zack6yuan@gmail.com>
2024-10-14 03:50:35 +0000

    update task 7.

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -2,6 +2,6 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
     id INT UNIQUE AUTO INCREMENT NOT NULL PRIMARY KEY;
-    state_id INT NOT NULL FOREIGN KEY(id.states);
+    state_id INT NOT NULL FOREIGN KEY(state_id) REFERENCES (state_id);
     name VARCHAR(256) NOT NULL;
 )
