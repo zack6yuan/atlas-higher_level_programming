e72331c5a0d2a89673826f1208f1b6a1208b8ba0
zack6yuan <zack6yuan@gmail.com>
2024-10-14 03:35:27 +0000

    update task 7.

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,7 @@
+-- Creates the database "hbtn_0d_usa" and the table "cities" in your MySQL server.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+    id INT UNIQUE AUTO INCREMENT NOT NULL PRIMARY KEY;
+    state_id INT NOT NULL FOREIGN KEY(id.states);
+    name VARCHAR(256) NOT NULL;
+)
