c2329e359be20e0b6db19454262cd65471c17e23
zack6yuan <zack6yuan@gmail.com>
2024-10-15 02:46:47 +0000

    capitalize.

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- Lists all cities contained in the databse "hbtn_0d_usa"
-SELECT cities.id, cities.name, states.name from cities
+SELECT cities.id, cities.name, states.name FROM cities
 WHERE cities.state_id = states.id
 ORDER BY cities.id;
