ab85f76c25e590f97daa61e2a2d77fb2cf8ed4d2
zack6yuan <zack6yuan@gmail.com>
2024-10-15 14:50:34 +0000

    update task 1.

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    """
+    Method:
+        list all states with a name starting with "N" (uppercase)
+        from the database "hbtn_0e_0_usa"
+    Attributes:
+        username = first argument
+        password = second argument
+        database name = third argument
+    """
+    db = MySQL.connect(
+        host="localhost"
+        
+    )
