72c0eb5d9b772e0bbe7687592885b71526db9819
zack6yuan <zack6yuan@gmail.com>
2024-10-15 02:48:54 +0000

    update task 9.

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- Lists all cities contained in the databse "hbtn_0d_usa"
 SELECT cities.id, cities.name, states.name FROM cities
-WHERE cities.state_id = states.id
+JOIN cities.state_id = states.id
 ORDER BY cities.id;
