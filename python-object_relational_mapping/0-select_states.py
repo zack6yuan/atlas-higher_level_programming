57be12df4b8dd53eddf6e020e9aa74303699ed09
zack6yuan <zack6yuan@gmail.com>
2024-10-15 14:50:39 +0000

    update task 0.

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -14,7 +14,8 @@ if __name__ == "__main__":
     """
     db = MySQLdb.connect(
         host="localhost",
-        port=3306, user=sys.argv[1],
+        port=3306,
+        user=sys.argv[1],
         password=sys.argv[2],
         db=sys.argv[3]
         )  # connect to MySQL database.
