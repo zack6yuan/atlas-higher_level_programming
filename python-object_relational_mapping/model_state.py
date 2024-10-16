d53b36fde29094db2b35aaae9075c72a8521e64f
zack6yuan <zack6yuan@gmail.com>
2024-10-16 02:06:20 +0000

    update state model.

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -4,12 +4,14 @@ and an instance Base = declarative_base() """
 from sqlalchemy import Integer  # used for integer columns
 from sqlalchemy import Column  # used to create columns
 from sqlalchemy import String  # defines string columns
-from sqlalchemy.ext.declarative import declarative_base  # used to create base class
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+
 
 class State(Base):
     """ Method" Initialize class State that inherits from Base """
     __tablename__ = "states"
     id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
+    
