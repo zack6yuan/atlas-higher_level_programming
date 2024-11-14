64534d25f83b134b66109e96d769b276f1e7e57e
zack6yuan <zack6yuan@gmail.com>
2024-11-14 10:22:43 +0000

    changed tab size.

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 // Node.js script based on arguments passed
 if (process.argv.length < 3) {
-    console.log('No argument');
+  console.log('No argument');
 } else if (process.argv.length === 3) {
-    console.log('Argument found');
+  console.log('Argument found');
 } else {
-    console.log('Arguments found');
+  console.log('Arguments found');
 }
 
