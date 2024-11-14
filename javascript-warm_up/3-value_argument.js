eec96d4409dc62c39b39dcf4c0ba4ad8c45020ac
zack6yuan <zack6yuan@gmail.com>
2024-11-14 10:29:16 +0000

    update 3 for prep.

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
-// Node.js scripr that prints the first arg passed
+// Node.js script that prints the first arg passed
 if (process.argv.length < 3) {
   console.log('No argument')
 } else {
   console.log(process.argv[2])
 }
+// can't use 'length'
