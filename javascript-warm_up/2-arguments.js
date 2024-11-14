2875cd92b17f0fa0d4c99eed9a23ba94ddcec26b
zack6yuan <zack6yuan@gmail.com>
2024-11-14 06:25:15 +0000

    update task 2.

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+if (arguments.length == 0){
+    console.log('No argument');
+} else if (arguments.length == 1) {
+    console.log('Argument found');
+} else {
+    console.log('Arguments found');
+}
