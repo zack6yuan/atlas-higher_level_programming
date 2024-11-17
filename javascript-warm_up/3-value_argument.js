992548c8c0dff515b70297602134104a6f971461
zack6yuan <zack6yuan@gmail.com>
2024-11-17 23:59:04 +0000

    oh baby a triple.

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // Node.js script that prints the first arg passed
-if (process.argv[2] == undefined) {
+if (process.argv[2] === undefined) {
   console.log('No argument');
 } else {
   console.log(process.argv[2]);
