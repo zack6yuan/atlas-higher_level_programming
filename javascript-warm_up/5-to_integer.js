6498b6c088cd70fd00bf5b353b14ec2ddda48417
zack6yuan <zack6yuan@gmail.com>
2024-11-19 00:57:32 +0000

    update task 5.

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,2 +1,8 @@
 #!/usr/bin/node
 // Node.js script that prints to console.
+const check = parseInt(process.argv[2]);
+if (isNaN(check)) {
+  console.log('Not a number');
+} else {
+  console.log(`My number: ${check}`)
+}
