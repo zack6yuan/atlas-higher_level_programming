82681a90dcde11fc3e7a74f4759811cd53911b2d
zack6yuan <zack6yuan@gmail.com>
2024-11-19 16:50:17 +0000

    doc.

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 // Node.js script that computes + prints a factorial.
+// Factorial - all the numbers < value but > 0.
 const value = parseInt(process.argv[2]);
 function getFactorial (value) {
   if (isNaN(value) || value === 0) {
