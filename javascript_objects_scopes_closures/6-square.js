5ee8e17ef68cce99c1f0771034952f9c10a9aa8b
zack6yuan <zack6yuan@gmail.com>
2024-11-19 19:57:19 +0000

    update tasks 6 and 8.

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// Node.js script that defines a square
+const Task5Square = require('./5-square');
+class Square extends Task5Square {
+  
+}
+
+module.exports = Square;
---
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Node.js script that returns reversed list
+exports.esrever = function (list) {
+  const reverse = [];
+  // iterate over normal list
+  for (let x = 0; x < list.length; x++) {
+    reverse[x] = list[list.length - 1 - x];
+  }
+  return reverse;
+}
