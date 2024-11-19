9ad6ad42410aef63d87a65b9e9543ce9c99c4077
zack6yuan <zack6yuan@gmail.com>
2024-11-19 20:09:27 +0000

    semistandard.

--- javascript_objects_scopes_closures/10-converter.js
+++ javascript_objects_scopes_closures/10-converter.js
@@ -2,7 +2,7 @@
 // Node.js script that converts a number
 // From base 10 to another base (argument)
 exports.converter = function (base) {
-  return function myConverter(x) {
+  return function myConverter (x) {
     return x.toString(base);
   };
 };
--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -2,7 +2,7 @@
 // Node.js script that defines a square
 const Task5Square = require('./5-square');
 class Square extends Task5Square {
-  
+
 }
 
 module.exports = Square;
