40546e25114a634b59079e8b8548f2e5a9990106
zack6yuan <zack6yuan@gmail.com>
2024-11-19 16:35:38 +0000

    semistandard.

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // Node.js script that computes + prints a factorial.
 const value = parseInt(process.argv[2]);
-function getFactorial(value) {
+function getFactorial (value) {
   if (isNaN(value) || value === 0) {
     return 1;
   } else {
