d45258573b7d9432716b24245ea698b18f39711c
zack6yuan <zack6yuan@gmail.com>
2024-11-19 00:58:07 +0000

    semicolon.

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -4,5 +4,5 @@ const check = parseInt(process.argv[2]);
 if (isNaN(check)) {
   console.log('Not a number');
 } else {
-  console.log(`My number: ${check}`)
+  console.log(`My number: ${check}`);
 }
