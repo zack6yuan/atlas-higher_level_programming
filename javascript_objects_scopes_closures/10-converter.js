1ebc7fae660a0757735ce909833339841dfa12fe
zack6yuan <zack6yuan@gmail.com>
2024-11-19 20:08:47 +0000

    update task 10.

---
+++ javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// Node.js script that converts a number
+// From base 10 to another base (argument)
+exports.converter = function (base) {
+  return function myConverter(x) {
+    return x.toString(base);
+  };
+};
