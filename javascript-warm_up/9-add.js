893b399856767eb7f856658aacb08a4464ffceb9
zack6yuan <zack6yuan@gmail.com>
2024-11-19 01:47:29 +0000

    update task 9.

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,2 +1,11 @@
 #!/usr/bin/node
 // Node.js script that adds two integers.
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
+
+function add(a, b) {
+  return (a + b);
+}
+
+let sum = a + b;
+console.log(sum);
