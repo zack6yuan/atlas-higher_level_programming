b6802f6008d16ae692839c1c5b2155c18c380f91
zack6yuan <zack6yuan@gmail.com>
2024-11-19 16:05:32 +0000

    semistandard.

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
 // Node.js script that prints x times "C is fun"
 const message = 'C is fun';
-let iterations = parseInt(process.argv[2]);
+const iterations = parseInt(process.argv[2]);
 if (isNaN(iterations)) {
-  console.log('Not a number')
+  console.log('Not a number');
 } else {
   for (let x = 0; x < iterations; x++) {
     console.log(message);
-}}
+  }
+}
