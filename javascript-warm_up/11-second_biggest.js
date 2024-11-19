86fe7c838e415afdb02b2504727680b676860baf
zack6yuan <zack6yuan@gmail.com>
2024-11-19 16:45:47 +0000

    update task 13.

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,2 +1,8 @@
 #!/usr/bin/node
 // Node.js script that searches integers in a list.
+const value = parseInt(process.argv[2]);
+if (isNan(value) || isNan(process.argv[3])) {
+  console.log('0')
+} else {
+  let 
+}
--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,2 +1,6 @@
 #!/usr/bin/node
 // Node.js script that adds two integers.
+function add (a, b) {
+  return a + b;
+}
+module.exports.add = add;
