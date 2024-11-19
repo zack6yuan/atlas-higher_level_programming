ddbad9cce442e66807c446eeeb18baf434859384
zack6yuan <zack6yuan@gmail.com>
2024-11-19 01:03:53 +0000

    update task 6.

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,2 +1,6 @@
 #!/usr/bin/node
 // Node.js script that prints three lines.
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let x = 0; x < array.length; x++) {
+  console.log(array[x]);
+}
