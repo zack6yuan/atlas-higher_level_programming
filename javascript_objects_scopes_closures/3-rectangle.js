609d5d3755d8f36a5e345548f1e54999ba7b5aec
zack6yuan <zack6yuan@gmail.com>
2024-11-19 17:21:09 +0000

    created files.

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// Node.js script that defines a rectangle
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+    print() {
+      for (let x = 0; x < this.height; x++) {
+        
+      }
+    }
+  }
+}
+module.exports = Rectangle;
