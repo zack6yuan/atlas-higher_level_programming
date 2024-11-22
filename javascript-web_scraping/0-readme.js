b0a1f07ccb7836afdfbd1162ebb3f8659868f856
zack6yuan <zack6yuan@gmail.com>
2024-11-22 03:45:29 +0000

    permissions and task 0.

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,2 +1,14 @@
 #!/usr/bin/node
 // Node.js script that reads and prints file contents.
+// Use file system module for operations.
+const fs = require('fs');
+const file_name = process.argv[2];
+fs.readFIle(file_name, 'utf-8', (err, data) => {
+  if (err) {
+    // print error object.
+    console.log(err);
+  } else {
+    // print data.
+    console.log(data);
+  }
+});
