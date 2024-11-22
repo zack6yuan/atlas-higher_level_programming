a220d0955a46b80997bc3b8b69a4a179372cd718
zack6yuan <zack6yuan@gmail.com>
2024-11-22 03:55:09 +0000

    camelCase.

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -2,8 +2,8 @@
 // Node.js script that reads and prints file contents.
 // Use file system module for operations.
 const fs = require('fs');
-const file_name = process.argv[2];
-fs.readFile(file_name, 'utf-8', (err, data) => {
+const fileName = process.argv[2];
+fs.readFile(fileName, 'utf-8', (err, data) => {
   if (err) {
     // print error object.
     console.log(err);
