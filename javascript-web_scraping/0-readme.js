0eb193b5a1715391d2e6369da7065eb816d4bd11
zack6yuan <zack6yuan@gmail.com>
2024-11-22 04:01:07 +0000

    update task 1.

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -2,8 +2,8 @@
 // Node.js script that reads and prints file contents.
 // Use file system module for operations.
 const fs = require('fs');
-const fileName = process.argv[2];
-fs.readFile(fileName, 'utf-8', (err, data) => {
+const filePath = process.argv[2];
+fs.readFile(filePath, 'utf-8', (err, data) => {
   if (err) {
     // print error object.
     console.log(err);
--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,2 +1,13 @@
 #!/usr/bin/node
 // Node.js script that writes a string to a file.
+// Use file system module for operations.
+const fs = require('fs');
+const filePath = process.argv[2];
+const stringWrite = process.argv[3];
+fs.writeFIle(filePath, stringWrite, 'utf-8', (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data)
+  }
+});
