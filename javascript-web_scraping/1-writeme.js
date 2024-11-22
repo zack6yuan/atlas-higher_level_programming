5481afac345d6e2d8e05776fcd01d51a71a72604
zack6yuan <zack6yuan@gmail.com>
2024-11-22 04:02:09 +0000

    define data.

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -4,7 +4,7 @@
 const fs = require('fs');
 const filePath = process.argv[2];
 const stringWrite = process.argv[3];
-fs.writeFile(filePath, stringWrite, 'utf-8', (err) => {
+fs.writeFile(filePath, stringWrite, 'utf-8', (err, data) => {
   if (err) {
     console.log(err);
   } else {
