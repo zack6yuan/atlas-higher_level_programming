cead71f6858fe5b0901f480e6fdcf3ebf3a6a95f
zack6yuan <zack6yuan@gmail.com>
2024-11-22 23:23:53 +0000

    creating files.

---
+++ javascript-web_jquery/0-script.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+// Updates text color of <header> element to red (#FF0000)
+const element = document.querySelector('header');
+header.style.color = '#FF0000';
--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -2,3 +2,6 @@
 /* Node.js script that prints the title of a
 Star Wars movie where the episode number
 matches a given integer */
+const getRequest = require('request');
+api = 'https://swapi-api.hbtn.io/api/films/';
+getRequest(api, )
