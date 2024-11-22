41c1da6606e551afd31adf7951e1d5bf8a6ec5ad
zack6yuan <zack6yuan@gmail.com>
2024-11-22 04:01:36 +0000

    syntax error.

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -4,7 +4,7 @@
 const fs = require('fs');
 const filePath = process.argv[2];
 const stringWrite = process.argv[3];
-fs.writeFIle(filePath, stringWrite, 'utf-8', (err) => {
+fs.writeFile(filePath, stringWrite, 'utf-8', (err) => {
   if (err) {
     console.log(err);
   } else {
