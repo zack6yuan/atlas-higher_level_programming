596cae5c87a624df0af4b75bc7548a1d4fac85f3
zack6yuan <zack6yuan@gmail.com>
2024-11-22 23:10:29 +0000

    update task 2.

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,3 +1,11 @@
 #!/usr/bin/node
 /* Node.js script that displays the status
 code of a GET request */
+const getRequest = require('request');
+getRequest(process.argv[2], (err, response) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`code: ${response.status}`)
+  }
+});
