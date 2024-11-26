15ee058d1a0c100406273862bd82634307cfde98
zack6yuan <zack6yuan@gmail.com>
2024-11-26 01:15:24 +0000

    update task 6.

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -2,3 +2,8 @@
 /* Node.js script that computes the number
 of tasks completed by user id */
 const getRequest = require('request');
+const fs = require('fs');
+const apiURL = 'https://jsonplaceholder.typicode.com/todos' + process.argv[2];
+getRequest(apiURL, (error, response, body) => {
+  if (err) {
+    console.log(err);)
