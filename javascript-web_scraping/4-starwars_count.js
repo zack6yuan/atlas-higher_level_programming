afcd306c3c240b81d41f6e203c437cd326f5c983
zack6yuan <zack6yuan@gmail.com>
2024-11-26 01:02:18 +0000

    update task 5.

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -2,3 +2,13 @@
 /* Node.js script that prints the number of
 movies where the charactger "Wedge Antilles"
 is present */
+const getRequest = require('request');
+// Build the URL for the data. + (First argument)
+const api = 'https://swapi-api.hbtn.io/api/films' + process.argv[2];
+getRequest(api, (err, response, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+
+  }
+});
--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,3 +1,19 @@
 #!/usr/bin/node
 /* Node.js script that gets the contents of
 a webpage and stores it in a file */
+const getRequest = require('request');
+// Use file system module for operations.
+const fs = require('fs')
+const url = process.argv[2];
+const path = process.argv[3];
+getRequest(url, (error, response, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    fs.writeFile(path, body, 'utf-8', err) => {
+      if (err) {
+        console.log(err);
+      }
+    }
+  }
+});
