3861d151bc1d5e61da6da05c558473f3061f80bc
zack6yuan <zack6yuan@gmail.com>
2024-11-26 01:25:55 +0000

    update task 1.

--- javascript-web_jquery/1-script.js
+++ javascript-web_jquery/1-script.js
@@ -1,3 +1,5 @@
 #!/usr/bin/node
 // Updates text color of <header> element to red.
 // Using the JQuery API.
+const headerElement = document.querySelector('header');
+$('headerElement').css('color', '#FF0000');
