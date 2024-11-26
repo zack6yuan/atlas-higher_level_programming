b53c088def15744de5cac561529def3a8c8507f3
zack6yuan <zack6yuan@gmail.com>
2024-11-26 02:33:59 +0000

    update task 2.

--- javascript-web_jquery/2-script.js
+++ javascript-web_jquery/2-script.js
@@ -1,3 +1,6 @@
 #!/usr/bin/node
 // Updates the text color of the <header> element to red.
 // Using the JQuery API (onclick(DIV#red_header)).
+$('DIV#red_header').click(function () {
+  $('header').css('color', '#FF0000');
+});
