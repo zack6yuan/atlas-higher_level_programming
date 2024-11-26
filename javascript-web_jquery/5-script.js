1f0e10117430115eea64aac3d0b08cf3166a4b7a
zack6yuan <zack6yuan@gmail.com>
2024-11-26 14:55:10 +0000

    update task 5.

--- javascript-web_jquery/5-script.js
+++ javascript-web_jquery/5-script.js
@@ -1,3 +1,6 @@
 #!/usr/bin/node
 // Adds a <li> element to a list
 // Using the JQuery API (onclick(DIV#add_item)).
+$('DIV#add_item').click(function () {
+  $('UL.my_list').append('<li>Item</li>');
+})
