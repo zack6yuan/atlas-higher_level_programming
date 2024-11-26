9450f9a4faf21ee89950cc0bdf5b681656b5b613
zack6yuan <zack6yuan@gmail.com>
2024-11-26 14:50:03 +0000

    update task 4 toggle.

--- javascript-web_jquery/4-script.js
+++ javascript-web_jquery/4-script.js
@@ -1,3 +1,6 @@
 #!/usr/bin/node
 // Toggles the class of the <header> element
 // Using the JQuery API (onclick(DIV#toggle_header)).
+$('DIV#toggle_header').click(function () {
+  $('header').toggleClass('red green');
+})
