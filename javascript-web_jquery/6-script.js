5f9645fc004556a7d4f2d7b859a9e2befd311828
zack6yuan <zack6yuan@gmail.com>
2024-11-26 02:49:27 +0000

    update task 6.

--- javascript-web_jquery/6-script.js
+++ javascript-web_jquery/6-script.js
@@ -1,3 +1,6 @@
 #!/usr/bin/node
 // Updates the text of the <header> element.
 // Using the JQuery API (onclick(DIV#update_header)).
+$('DIV#update_header').click(function () {
+  $('header').text('New Header!!!');
+});
