3f029161a17bb073b83d333a71befca393e4891b
zack6yuan <zack6yuan@gmail.com>
2024-11-26 02:42:54 +0000

    update task 3.

--- javascript-web_jquery/3-script.js
+++ javascript-web_jquery/3-script.js
@@ -1,3 +1,6 @@
 #!/usr/bin/node
 // Adds the class "red" to the <header> element.
 // Using the JQuery API (onclick(DIV#red_header)).
+$('DIV#red_header').click(function () {
+  $('header').addClass('red');
+});
