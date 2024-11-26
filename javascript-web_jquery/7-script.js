9a104b148d9b0f5c357f26fe05eca379548ae6df
zack6yuan <zack6yuan@gmail.com>
2024-11-26 16:55:41 +0000

    update tasks 7 and 8.

--- javascript-web_jquery/7-script.js
+++ javascript-web_jquery/7-script.js
@@ -1,3 +1,6 @@
 #!/usr/bin/node
 // Fetches the character "name" from URL
 // Using the JQuery API.
+$.get('https://swapi-api.hbtn.io/api/people/5/?format=json', function (data) {
+  $('DIV#character').text(data.name);
+});
--- javascript-web_jquery/8-script.js
+++ javascript-web_jquery/8-script.js
@@ -1,3 +1,6 @@
 #!/usr/bin/node
 // Fetches and lists the title for all movies.
 // Using the JQuery API.
+$.get('https://swapi-api.hbtn.io/api/films/?format=json', function (data) {
+  $('UL#list_movies').append(`<li>${data.title}</li>`);
+});
