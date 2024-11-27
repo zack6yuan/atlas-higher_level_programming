d3603a74bdab9c6f1eca053331fcaa429f9b33a8
zack6yuan <zack6yuan@gmail.com>
2024-11-26 21:35:12 -0600

    updates.

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -3,7 +3,7 @@
 a webpage and stores it in a file */
 const getRequest = require('request');
 // Use file system module for operations.
-const fs = require('fs')
+const fs = require('fs');
 const url = process.argv[2];
 const path = process.argv[3];
 getRequest(url, (error, response, body) => {
@@ -14,7 +14,7 @@ getRequest(url, (error, response, body) => {
       if (error) {
         console.log(error);
       } else {
-        const jsonData = JSON.parse(data);
+        const jsonData = JSON.parse(jsonData);
         console.log(jsonData);
       }
     });
